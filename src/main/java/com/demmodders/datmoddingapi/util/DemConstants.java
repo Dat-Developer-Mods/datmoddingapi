c37c4004cd867551687554e371b250b8c88fad8d
Jacob <jtljac2@gmail.com>
2020-05-19 01:33:46 +0100

    Incremented version, added colour constants for player status

--- src/main/java/com/demmodders/datmoddingapi/util/DemConstants.java
+++ src/main/java/com/demmodders/datmoddingapi/util/DemConstants.java
@@ -9,4 +9,10 @@ public final class DemConstants {
         public static final TextFormatting COMMAND = TextFormatting.DARK_PURPLE;
         public static final TextFormatting HEADER = TextFormatting.AQUA;
     }
+
+    public static final class playerColour {
+        public static final TextFormatting ONLINE = TextFormatting.GREEN;
+        public static final TextFormatting OFFLINE = TextFormatting.RED;
+        public static final TextFormatting AWAY = TextFormatting.GOLD;
+    }
 }
